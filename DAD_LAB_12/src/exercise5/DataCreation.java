664d98284320e7d0758453ddf56ee6577e5fb674
Khalid <82488402+KhalidAliFarah@users.noreply.github.com>
2023-07-13 21:23:44 +0800

    Update DataCreation.java
    I have add some comments in to the code.

--- DAD_LAB_12/src/exercise5/DataCreation.java
+++ DAD_LAB_12/src/exercise5/DataCreation.java
@@ -4,15 +4,24 @@ import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+/**
+ * DataCreation: Generate rainfall data and write it to a binary file.
+ * @Author Khalid
+ */
+
 public class DataCreation {
 
+    /**
+     * Main method to generate rainfall data and write it to a binary file.
+     * @param args Command line arguments
+     */
     public static void main(String[] args) {
-        double[] rainfallData = {0.0, 4.0, 1.0, 0.0, 6.0, 19.0};
-        String fileName = "rainfall_data.bin";
+        double[] rainfallData = {0.0, 4.0, 1.0, 0.0, 6.0, 19.0}; // Rainfall data array
+        String fileName = "rainfall_data.bin"; // Name of the binary file
 
         try (DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(fileName))) {
             for (double rainfall : rainfallData) {
-                dataOutputStream.writeDouble(rainfall);
+                dataOutputStream.writeDouble(rainfall); // Write each rainfall value to the file
             }
             System.out.println("Data generation was successful..");
         } catch (IOException e) {
