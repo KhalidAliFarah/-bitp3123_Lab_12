8cc799d857a84098e9a8b01e20072e50ca86a0d8
Khalid <82488402+KhalidAliFarah@users.noreply.github.com>
2023-07-13 21:34:45 +0800

    Update ReDataCreation.java
    I have add some comments on the code.

--- DAD_LAB_12/src/exercise7/ReDataCreation.java
+++ DAD_LAB_12/src/exercise7/ReDataCreation.java
@@ -1,22 +1,28 @@
 package exercise7;
 
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * ReDataCreation: Generate rainfall data and write them to a text file.
+ * @Author Khalid
+ */
+
 public class ReDataCreation {
 
     public static void main(String[] args) {
-        double[] rainfallData = {0.0, 4.0, 1.0, 0.0, 6.0, 19.0};
-        String fileName = "rainfall_data.txt";
+        double[] rainfallData = {0.0, 4.0, 1.0, 0.0, 6.0, 19.0}; // Rainfall data values
+        String fileName = "rainfall_data.txt"; // Name of the text file
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            // Write rainfall data to the text file
             for (double rainfall : rainfallData) {
-                writer.write(String.valueOf(rainfall));
-                writer.newLine();
+                writer.write(String.valueOf(rainfall)); // Write each rainfall data value
+                writer.newLine(); // Write a new line
             }
-            System.out.println("Data generation was successful..");
+
+            System.out.println("Data generation was successful.");
         } catch (IOException e) {
             System.out.println("An error occurred while creating the data: " + e.getMessage());
         }
