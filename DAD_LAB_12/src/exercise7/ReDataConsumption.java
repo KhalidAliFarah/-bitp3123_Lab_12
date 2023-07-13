4734e8919684cdbbbd2ad52e16dcb016ad1e753c
Khalid <82488402+KhalidAliFarah@users.noreply.github.com>
2023-07-13 21:32:03 +0800

    Update ReDataConsumption.java
    I have add some comments into the code.

--- DAD_LAB_12/src/exercise7/ReDataConsumption.java
+++ DAD_LAB_12/src/exercise7/ReDataConsumption.java
@@ -4,10 +4,18 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * ReDataConsumption: Read and process rainfall data from a text file.
+ * @Author Khalid
+ */
+
 public class ReDataConsumption {
     private double[] rainfallData;
     private double average;
 
+    /**
+     * Calculate the average rainfall from the rainfall data.
+     */
     public void calculateAverage() {
         double sum = 0.0;
         for (double rainfall : rainfallData) {
@@ -16,6 +24,9 @@ public class ReDataConsumption {
         average = sum / rainfallData.length;
     }
 
+    /**
+     * Display the rainfall data and average rainfall.
+     */
     public void displayData() {
         System.out.println("Rainfall Data:");
         for (double rainfall : rainfallData) {
@@ -25,10 +36,10 @@ public class ReDataConsumption {
     }
 
     public static void main(String[] args) {
-        String fileName = "rainfall_data.txt";
+        String fileName = "rainfall_data.txt"; // Name of the text file
 
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-        	ReDataConsumption rainfallData = new ReDataConsumption();
+            ReDataConsumption rainfallData = new ReDataConsumption();
 
             int dataSize = 6; // Assuming the data size is known
 
@@ -36,7 +47,7 @@ public class ReDataConsumption {
             for (int i = 0; i < dataSize; i++) {
                 String line = reader.readLine();
                 if (line != null) {
-                    rainfallData.rainfallData[i] = Double.parseDouble(line);
+                    rainfallData.rainfallData[i] = Double.parseDouble(line); // Parse the line to a double value
                 }
             }
 
