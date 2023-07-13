81915565902d9819430ea922cee65c77d180d69c
Khalid <82488402+KhalidAliFarah@users.noreply.github.com>
2023-07-13 21:26:24 +0800

    Update FirstClassDataCreation.java
    I have add some comments into the code.

--- DAD_LAB_12/src/exercise6/FirstClassDataCreation.java
+++ DAD_LAB_12/src/exercise6/FirstClassDataCreation.java
@@ -4,9 +4,14 @@ import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+/**
+ * FirstClassDataCreation: Generate rainfall data for multiple stations and write them to a binary file.
+ * @Author Khalid
+ */
+
 public class FirstClassDataCreation {
     public static void main(String[] args) {
-        String fileName = "rainfall_data.bin";
+        String fileName = "rainfall_data.bin"; // Name of the binary file
 
         try (DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(fileName))) {
             // Station 1: Simpang Ampat, Alor Gajah
@@ -15,7 +20,7 @@ public class FirstClassDataCreation {
             String district1 = "Alor Gajah"; // District name
             double[] rainfallData1 = {0.0, 4.0, 1.0, 0.0, 6.0, 19.0}; // Rainfall data for Station 1
 
-            // Write station information
+            // Write station information for Station 1
             dataOutputStream.writeInt(stationId1); // Write the station ID
             dataOutputStream.writeUTF(stationName1); // Write the station name
             dataOutputStream.writeUTF(district1); // Write the district name
@@ -31,7 +36,7 @@ public class FirstClassDataCreation {
             String district2 = "Jasin"; // District name
             double[] rainfallData2 = {0.0, 0.0, 1.0, 1.0, 69.0, 53.0}; // Rainfall data for Station 2
 
-            // Write station information
+            // Write station information for Station 2
             dataOutputStream.writeInt(stationId2); // Write the station ID
             dataOutputStream.writeUTF(stationName2); // Write the station name
             dataOutputStream.writeUTF(district2); // Write the district name
